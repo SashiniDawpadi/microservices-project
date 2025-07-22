c9ea0c6fa09085b82dd66df66f636e1ebf812d13
SashiniDawpadi <sashinidawpadi@gmail.com>
2025-07-22 13:46:53 +0530

    connected to docker | added interfaces to entity, repository

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -11,9 +11,14 @@ namespace Play.Catalog.Service.Controllers
 {
     [ApiController]
     [Route("items")]
-    public class ItemController : ControllerBase
+    public class ItemsController : ControllerBase
     {
-       private readonly ItemsRepository itemsRepository = new();
+       private readonly IRepository<Item> itemsRepository;
+
+       public ItemsController(IRepository<Item> itemsRepository)
+       {
+        this.itemsRepository = itemsRepository;
+       }
 
         [HttpGet]
         public async Task<IEnumerable<ItemDto>> GetAsync()
